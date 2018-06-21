d31031fc2ac1519d3dfe84602c652bc4e4011d7f
unknown <zoran@suto.ro>
2018-06-22 02:59:41 +0300

    her

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ app.get('/', (req, res) => {
     res.send('Invalid Endpoint!');
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Start the server
 server.listen(port, () => {
